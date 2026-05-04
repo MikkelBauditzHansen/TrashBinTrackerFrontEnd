fb85485a1c6265f720f8bdc2876fd60954a1aefa
MikkelBauditzHansen <mih016@edu.zealand.dk>
2026-05-04 13:01:31 +0200

    Add notifications functionality to Vue app

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const baseUrl = "https://localhost:7159/api/TrashBinTracker";
 const locationUrl = "https://localhost:7159/api/Location";
+const notificationUrl = "https://localhost:7159/api/Notification";
 
 Vue.createApp({
     data() {
         return {
             bins: [],
             locations: [],
+            notifications: [],
             newLocation: "",
             showForm: false,
 
@@ -22,11 +24,17 @@ Vue.createApp({
                 wasteType: "",
                 locationId: "",
                 fillLevel: 0
-            },
-
-            message: "",
-            error: ""
+            }
         };
+    },
+
+    computed: {
+        latestNotifications() {
+            return this.notifications
+                .slice()
+                .sort((a, b) => b.notificationId - a.notificationId)
+                .slice(0, 3);
+        }
     },
 
     methods: {
@@ -35,14 +43,10 @@ Vue.createApp({
             this.showForm = !this.showForm;
         },
 
+        // ---------------- BINS ----------------
         async getAllBins() {
             const res = await axios.get(baseUrl);
             this.bins = res.data;
-        },
-
-        async getLocations() {
-            const res = await axios.get(locationUrl);
-            this.locations = res.data;
         },
 
         async addBin() {
@@ -59,44 +63,6 @@ Vue.createApp({
             this.showForm = false;
         },
 
-        async addLocation() {
-            if (!this.newLocation) return;
-
-            const res = await axios.post(locationUrl, {
-                name: this.newLocation,
-                isIndoor: false
-            });
-
-            this.locations.push(res.data);
-
-            // auto vælg ny
-            this.newBin.locationId = res.data.id;
-
-            this.newLocation = "";
-        },
-
-        async updateLocation(loc) {
-            await axios.put(`${locationUrl}/${loc.id}`, loc);
-        },
-
-        async deleteLocation(id) {
-            try {
-                await axios.delete(`${locationUrl}/${id}`);
-                this.locations = this.locations.filter(l => l.id !== id);
-            } catch (err) {
-                alert(err.response?.data || "Kan ikke slette lokation");
-            }
-        },
-
-        startEdit(bin) {
-            this.editId = bin.id;
-            this.editBin = { ...bin };
-        },
-
-        cancelEdit() {
-            this.editId = null;
-        },
-
         async saveEdit(id) {
             const res = await axios.put(`${baseUrl}/${id}`, this.editBin);
 
@@ -111,6 +77,49 @@ Vue.createApp({
 
             await axios.delete(`${baseUrl}/${bin.id}`);
             this.bins = this.bins.filter(b => b.id !== bin.id);
+        },
+
+        startEdit(bin) {
+            this.editId = bin.id;
+            this.editBin = { ...bin };
+        },
+
+        cancelEdit() {
+            this.editId = null;
+        },
+
+        // ---------------- LOCATIONS ----------------
+        async getLocations() {
+            const res = await axios.get(locationUrl);
+            this.locations = res.data;
+        },
+
+        async addLocation() {
+            if (!this.newLocation) return;
+
+            const res = await axios.post(locationUrl, {
+                name: this.newLocation,
+                isIndoor: false
+            });
+
+            this.locations.push(res.data);
+            this.newBin.locationId = res.data.id;
+            this.newLocation = "";
+        },
+
+        async updateLocation(loc) {
+            await axios.put(`${locationUrl}/${loc.id}`, loc);
+        },
+
+        async deleteLocation(id) {
+            await axios.delete(`${locationUrl}/${id}`);
+            this.locations = this.locations.filter(l => l.id !== id);
+        },
+
+        // ---------------- NOTIFICATIONS ----------------
+        async getNotifications() {
+            const res = await axios.get(notificationUrl);
+            this.notifications = res.data;
         },
 
         getLocationName(id) {
@@ -150,6 +159,7 @@ Vue.createApp({
     mounted() {
         this.getAllBins();
         this.getLocations();
+        this.getNotifications();
     }
 
 }).mount("#app");
