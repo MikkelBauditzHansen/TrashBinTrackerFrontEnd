66381a4c63b4d6d23fdb2c8075b5818f7ffa6cc6
FL03NG <sih013@edu.zealand.dk>
2026-05-12 13:47:09 +0200

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,48 @@
+Vue.createApp({
+
+    data() {
+        return {
+
+            authurl: "https://localhost:7159/api/auth/login",
+
+            auth: {
+                username: "",
+                password: ""
+            },
+
+            authMessage: ""
+        }
+    },
+
+    methods: {
+
+        async login() {
+
+            try {
+
+                const response = await axios.post(this.authurl, this.auth);
+
+                // save token
+                localStorage.setItem("token", response.data.token);
+
+                // save role
+                localStorage.setItem("role", response.data.role);
+
+                // save username
+                localStorage.setItem("username", this.auth.username);
+
+                // redirect
+                window.location.href = "Dashboard.html";
+
+            }
+
+            catch (error) {
+
+                this.authMessage = "Invalid username or password";
+
+                console.error(error);
+            }
+        }
+    }
+
+}).mount("#app");
